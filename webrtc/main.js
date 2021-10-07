39d43fe0642857fa6930232b43ad5dae1354ff98
wenjunche <wenjun@openfin.co>
2021-10-07 10:58:11 -0400

    added webrtc example

---
+++ webrtc/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const leftVideo = document.getElementById('leftVideo');
+const rightVideo = document.getElementById('rightVideo');
+
+leftVideo.addEventListener('canplay', () => {
+  let stream;
+  const fps = 0;
+  if (leftVideo.captureStream) {
+    stream = leftVideo.captureStream(fps);
+  } else if (leftVideo.mozCaptureStream) {
+    stream = leftVideo.mozCaptureStream(fps);
+  } else {
+    console.error('Stream capture is not supported');
+    stream = null;
+  }
+  rightVideo.srcObject = stream;
+});
